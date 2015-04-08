054c1bb50d02d7fb6e3366b6cbc5fa171864fe9b
Mary <user@users-MacBook-Pro-3.local>
2015-04-08 15:31:53 -0400

    added route and erb for new contacts

--- crm.rb
+++ crm.rb
@@ -2,6 +2,9 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
+#routes
 get '/' do
   @t = Time.now
   @crm_app_name = "My CRM"
@@ -9,15 +12,10 @@ get '/' do
 end
 
 get '/contacts' do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
   erb :contacts
 end
 
 get '/contacts/new' do
-  erb :index
+  erb :new_contact
 end
 
