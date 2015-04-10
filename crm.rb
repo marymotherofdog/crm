19bea8f9159cf9d14e25c2cd392d39c3b8f4fa62
Mary <user@users-MacBook-Pro-3.local>
2015-04-10 15:41:22 -0400

    Added edit_contact.erb

--- crm.rb
+++ crm.rb
@@ -24,7 +24,20 @@ end
 
 get "/contacts/:id" do
   @contact = $rolodex.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 
