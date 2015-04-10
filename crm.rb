d1a551df2f43ec9e52e19c1d1216b755077f2720
Mary <user@users-MacBook-Pro-3.local>
2015-04-10 14:55:08 -0400

    added view for individual contact

--- crm.rb
+++ crm.rb
@@ -4,6 +4,10 @@ require 'sinatra'
 
 $rolodex = Rolodex.new
 
+$rolodex.add_contact(Contact.new("Eric", "Wareheim", "Eric@tim&eric.com", "Wayne Skyler"))
+
+
+
 #routes
 get '/' do
   @crm_app_name = "My CRM"
@@ -17,6 +21,12 @@ end
 get '/contacts/new' do
   erb :new_contact
 end
+
+get "/contacts/1" do
+  @contact = $rolodex.find(1)
+  erb :show_contact
+end
+
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
